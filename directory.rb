21f5654ebb17d618495059e95ac77718973103ee
CWong <canacewongtoday@hotmail.com>
2017-09-20 10:44:34 +0000

    first program

--- directory.rb
+++ directory.rb
@@ -1,13 +1,40 @@
-puts "The students of Villains Academy"
-puts "-----------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    
+    students = []
+    
+    name = gets.chomp 
+    #get the first input 
+    
+    while !name.empty? do 
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        
+        name = gets.chomp 
+    end 
+    
+    #return an array of students 
+    students
+end 
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
+end 
+
+def print(students)
+    students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+   puts "Overall, we have #{names.count} great students"
+end 
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
